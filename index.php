59f744c0e2aea06220f6fcba03b9ffa8b82f8d73
Lucas <83518257+Luqaska@users.noreply.github.com>
2022-10-08 00:53:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
 <h2><?= $trans["socials"]; ?></h2>
 <ul>
 	<li><a target="_blank" href="https://github.com/Luqaska">GitHub</a></li>
-	<li><a target="_blank" href="https://sns.koyu.space/@luqaska">Microblog<?= $trans["in_es"]; ?></a>
-	<li><a target="_blank" href="https://is.nota.live/@lucas">Microblog (<?= $trans["secondary"]; ?>)<?= $trans["in_es"]; ?></a>
-	<li><a target="_blank" href="https://twitter.com/Luqaska">Twitter <?= $trans["twitter_note"] . $trans["in_es"]; ?></a>
-	<li><a target="_blank" href="https://tumblr.atico.ga">Tumblr <?= $trans["tumblr_note"] . $trans["in_en"]; ?></a>
+	<li><a target="_blank" href="https://sns.koyu.space/@luqaska">Microblog<?= $trans["in_es"]; ?></a></li>
+	<li><a target="_blank" href="https://ieji.de/@Lu" rel="me">Microblog (<?= $trans["secondary"]; ?>)<?= $trans["in_es"]; ?></a></li>
+	<li><a target="_blank" href="https://is.nota.live/@lucas" rel="me">Microblog (<?= $trans["tertiary"]; ?>)<?= $trans["in_es"]; ?></a></li>
+	<li><a target="_blank" href="https://twitter.com/Luqaska">Twitter <?= $trans["twitter_note"] . $trans["in_es"]; ?></a></li>
+	<li><a target="_blank" href="https://tumblr.atico.ga">Tumblr <?= $trans["tumblr_note"] . $trans["in_en"]; ?></a></li>
 </ul>
 <?php include("_last.php");
