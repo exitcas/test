3b0326136aea15d68cd27a069874d38c68f8dac5
Lucas <83518257+exitcas@users.noreply.github.com>
2022-11-17 07:56:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <h2><?= $trans["socials"]; ?></h2>
 <ul>
 	<li><a target="_blank" href="https://github.com/exitcas">GitHub</a></li>
-	<li><a target="_blank" href="https://ieji.de/@Lu" rel="me">Microblog</a></li>
-	<li><a target="_blank" href="https://is.nota.live/@lucas" rel="me">Microblog (<?= $trans["secondary"]; ?>)</a></li>
-	<li><a target="_blank" href="https://koyu.space/@exitcas" rel="me">Koyu.Space</a></li>
-	<li><a target="_blank" href="https://www.plurk.com/atico">Plurk</a></li>
+	<li><a target="_blank" href="https://ieji.de/@Lu" rel="me">Microblog<?= $trans["in_en"]; ?></a></li>
+	<li><a target="_blank" href="https://is.nota.live/@lucas" rel="me">Microblog (<?= $trans["secondary"]; ?>)<?= $trans["in_en"]; ?></a></li>
+	<li><a target="_blank" href="https://koyu.space/@exitcas" rel="me">Koyu.Space<?= $trans["in_en"]; ?></a></li>
+	<li><a target="_blank" href="https://www.plurk.com/atico">Plurk<?= $trans["in_en"]; ?></a></li>
 	<li><a target="_blank" href="https://tumblr.atico.ga">Tumblr <?= $trans["tumblr_note"] . $trans["in_en"]; ?></a></li>
 </ul>
 <?php include("_last.php");
