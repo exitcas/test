722434c780aec0bfb4a23b54b95bd641ee2fda40
Lucas <83518257+exitcas@users.noreply.github.com>
2022-12-28 19:02:57 +0000

    Create _webfinger.php

---
+++ _webfinger.php
@@ -0,0 +1,9 @@
+<?php
+if (
+  isset($_GET["resource"])
+  &&
+  strtolower($_GET["resource"]) == ("cas@atico.ga" || "acct:cas@atico.ga")
+) {
+  header("Content-type: application/json");
+  echo '{"subject":"acct:cas@atico.ga","aliases":["https://ieji.de/@Lu","https://ieji.de/users/Lu"],"links":[{"rel":"http://webfinger.net/rel/profile-page","type":"text/html","href":"https://ieji.de/@Lu"},{"rel":"self","type":"application/activity+json","href":"https://ieji.de/users/Lu"},{"rel":"http://ostatus.org/schema/1.0/subscribe","template":"https://ieji.de/authorize_interaction?uri={uri}"}]}';
+}
